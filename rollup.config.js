76b3caa99d60197e30289b466f0aa936b6f28279
wlee221 <wlee221@live.unc.edu>
2020-09-20 19:31:42 -0700

    Add minimal repro for amplify-js#6826

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,29 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import json from "@rollup/plugin-json";
+import { terser } from "rollup-plugin-terser";
 
 // `npm run build` -> `production` is true
 // `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
-	output: {
-		file: 'public/bundle.js',
-		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
-		sourcemap: true
-	},
-	plugins: [
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
-	]
+  input: "src/main.js",
+  output: {
+    file: "public/bundle.js",
+    format: "umd", // immediately-invoked function expression — suitable for <script> tags
+    sourcemap: true,
+  },
+  plugins: [
+    json(),
+    commonjs({
+      include: "node_modules/**",
+    }), // converts date-fns to ES modules
+    resolve({
+      browser: true,
+      preferBuiltins: true,
+    }), // tells Rollup how to find date-fns in node_modules
+
+    production && terser(), // minify, but only in production
+  ],
 };
